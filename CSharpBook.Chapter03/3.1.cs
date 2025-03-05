14f352c87292b3596e490a6e557c000565622865
Absinthe <absinthe1311@gmail.com>
2025-03-05 11:43:22 +0800

    Chapter03

---
+++ CSharpBook.Chapter03/3.1.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//namespace CSharpBook.Chapter03
+//{
+//    class StatementTest
+//    {
+//        void PrintArea(int r)
+//        {
+//            const double PI = 3.14;
+//            double a;
+//            if( r> 0)
+//            {
+//                a = PI * r * r;
+//                Console.WriteLine("半径={0}，面积={1}", r, a);
+//            }
+//            else
+//            {
+//                Console.WriteLine("半径不能小于等于0");
+//            }
+//        }
+//        static void Main()
+//        {
+//            StatementTest obj;
+//            obj = new StatementTest();
+//            for(int i = -2; i<3;i++)
+//            {
+//                obj.PrintArea(i);
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ CSharpBook.Chapter03/3.2.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpBook.Chapter03
+//{
+//    class ArithmeticTest
+//    {
+//        static void Main()
+//        {
+//            double x, y;
+//            x = 1.5; y = ++x;
+//            Console.WriteLine("x = {0}, y = {1}", x, y);
+//            x = 1.5; y = x++;
+//            Console.WriteLine("x = {0}, y = {1}", x, y);
+//            x = 1.5; y = --x;
+//            Console.WriteLine("x = {0}, y = {1}", x, y);
+//            x = 1.5; y = x--;
+//            Console.WriteLine("x = {0}, y = {1}", x, y);
+//            x = 5.8; int i = 5;
+//            Console.WriteLine("i = {0}, +i = {1}", i, +i);
+//            Console.WriteLine("i+5={0}, i+.5={1}", i + 5, i + .5);
+//            Console.WriteLine("x={0}, x+ \"5\" = {1}", x, x + "5");
+//            Console.WriteLine("'5'+\"5\"={0}, '5'+5={1}", '5' + "5", '5' + 5);
+//            // 5+"5" 进行字符拼接 5+'5'是将'5'转换成ASCII码值
+//            i = 5;
+//            Console.WriteLine("i = {0}, -i = {1}, i -1 = {2}, i-.5 = {3}", i, -i, i - 1, i - .5);
+//            Console.ReadKey();
+//        }
+//    }
+//}
